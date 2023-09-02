4da1fc1c4459495e78f1658fe75f6d73c4a7b8cb
Jorge Guillen Rivera <jorgegrr052002@gmail.com>
2023-09-02 17:19:11 -0500

    1. Inicio de aplicación

--- lab1_20191660/app/src/main/java/com/example/lab1_20191660/MainActivity.java
+++ lab1_20191660/app/src/main/java/com/example/lab1_20191660/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.lab1_20191660;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar_inicio;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
+        textView = findViewById(R.id.TeleAhorcado);
+        registerForContextMenu(textView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        if (v.getId() == R.id.TeleAhorcado) {
+            getMenuInflater().inflate(R.menu.textview_context_menu, menu);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_item_azul) {
+            textView.setTextColor(getResources().getColor(android.R.color.holo_blue_dark));
+            return true;
+        } else if (id == R.id.menu_item_verde) {
+            textView.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
+            return true;
+        } else if (id == R.id.menu_item_rojo) {
+            textView.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
